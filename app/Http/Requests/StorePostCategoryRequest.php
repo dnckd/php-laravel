89a4a644e7becb319c6aedc4f74e9130fef585cf
JWC <dnckd18@nate.com>
2024-04-23 12:55:40 +0900

    feat: add PostCategory request

---
+++ app/Http/Requests/StorePostCategoryRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+class StorePostCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string'],
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(
+            [
+                'result' => false,
+                'message' => 'Validation errors',
+                'data' => $validator->errors(),
+            ],
+            ResponseAlias::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
