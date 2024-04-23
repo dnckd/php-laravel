9062dbd374a63a2ced78974af2d5dbb19ffce911
JWC <dnckd18@nate.com>
2024-04-23 12:56:33 +0900

    fix: add PostCategory api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\Category\PostCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('post-categories')->group(function () {
+    Route::get('/', [PostCategoryController::class, 'index']);
+    Route::post('/', [PostCategoryController::class, 'store']);
+    Route::get('{postCategory}', [PostCategoryController::class, 'show']);
+    Route::put('{postCategory}', [PostCategoryController::class, 'update']);
+    Route::delete('{postCategory}', [PostCategoryController::class, 'destroy']);
+});
